c93e3bff7c7c36b0332de0173b4919c7e7d84d5a
pistol-1 <martinvelandia27@gmail.com>
2025-05-20 13:24:34 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ import { VRButton } from 'three/addons/webxr/VRButton.js';
 
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.001, 1000);
     
-    camera.position.set(0.34, 1.135, -0.21);
+
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -15,7 +15,7 @@ renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
 renderer.xr.enabled = true;
-//renderer.xr.setReferenceSpaceType( 'local' );
+renderer.xr.setReferenceSpaceType( 'local' );
 document.body.appendChild( VRButton.createButton( renderer ) );;
 
     const light = new THREE.DirectionalLight(0xffffff, 1);
@@ -35,7 +35,7 @@ document.body.appendChild( VRButton.createButton( renderer ) );;
     camera.lookAt(cube.position)
 
 
-    
+        camera.position.set(0.34, 1.135, -0.21);
 
 // Load e30.glb
 let e30Model;
